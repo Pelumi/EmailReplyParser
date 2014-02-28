a1f37c410b98547d440b997586ec15db20dfd828
daniel-choi <daniel.choi@edlio.com>
2014-02-27 16:52:55 -0800

    fix signature regex to handle -- properly

--- src/main/java/com/edlio/emailreplyparser/EmailParser.java
+++ src/main/java/com/edlio/emailreplyparser/EmailParser.java
@@ -13,7 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class EmailParser {
 	
-	static final Pattern SIG_PATTERN = Pattern.compile( "((^Sent from my (\\s*\\w+){1,3}$)|(^-\\w|__|--|^\u2013|^\u2014))", Pattern.DOTALL);
+	static final Pattern SIG_PATTERN = Pattern.compile( "((^Sent from my (\\s*\\w+){1,3}$)|(^-\\w|^\\s?__|^\\s?--|^\u2013|^\u2014))", Pattern.DOTALL);
 	static final Pattern QUOTE_PATTERN = Pattern.compile("(^>+)", Pattern.DOTALL);
 	private static List<Pattern> compiledQuoteHeaderPatterns;
 	
